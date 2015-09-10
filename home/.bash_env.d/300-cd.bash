51bc9ac200414bfe83241f2ca534a61df60d004c
William Clemens <wesclemens@gmail.com>
2015-09-09 23:38:52 -0500

    Added AutoEnv script for Python venv

---
+++ home/.bash_env.d/300-cd.bash
@@ -0,0 +1,28 @@
+VIRTUAL_ENV_AUTO="False"
+function cd-wrapper {
+  # Change Directory
+  builtin cd $@
+
+  if [[ -n "$VIRTUAL_ENV" && "$VIRTUAL_ENV_AUTO" == "True" ]]; then
+    if [[ $(pwd) != $(dirname $VIRTUAL_ENV)* ]]; then
+      echo deactivate
+      deactivate
+    fi
+  fi
+
+  if [[ -z "$VIRTUAL_ENV" ]]; then
+    # Check if in a git directory
+    if git rev-parse &>/dev/null; then
+      local env_dir=$(git rev-parse --show-toplevel)/.venv
+      if [[ -f $env_dir/bin/activate ]]; then
+        echo activate
+        VIRTUAL_ENV_AUTO="True"
+        source $env_dir/bin/activate
+      fi
+    fi
+  fi
+}
+
+alias cd="cd-wrapper"
+
+cd-wrapper . 
