caf216f93049dc50eefa42f068448e53c057c4c4
William Clemens <wesclemens@gmail.com>
2020-04-27 23:14:42 -0500

    Updated proxy login to better handle username and password

--- profile.d/500-proxy-login.bash
+++ profile.d/500-proxy-login.bash
@@ -9,6 +9,8 @@ function urlencode() {
 
 function proxy-login() {
   local protocol=${1-ALL}
+  local host=${2-}
+  local user=${3-}
 
   local curr_settings=
   case $protocol in
@@ -24,60 +26,86 @@ function proxy-login() {
       ;;
   esac
 
-  # Get host
-  local host=
-  local curr_host=$( echo $curr_settings | sed -n "s/.*@\(.*\)$/\1/p")
-  if [[ -z "$curr_host" ]]; then
-    read -p "Host: " host
-  else
-    read -p "Host [${curr_host}]: " host
-  fi
-  if [[ -z "$host" ]]; then
-    host=$curr_host
-  fi
+  local REGEX="^(https?)://(([^:]*)(:([^@]*))?@)?([^:]*)(:(.*))?$"
+  # Capture Groups:  1 - Protocol   3 - User  5 - Password  6 - Host  8 - Port
+  # Example:  var=$(echo $curr_settings | sed -n -E "s|${REGEX}|\1|p")
 
+  # Get host
   if [[ -z "$host" ]]; then
-    1>&2 echo "Host cannot be blank"
-    return 1
+    local curr_port=$(echo $curr_settings | sed -n -E "s|${REGEX}|\8|p")
+    local curr_host=$(echo $curr_settings | sed -n -E "s|${REGEX}|\6|p")
+    if [[ -n "$curr_port" ]] && [[ -n "$curr_host" ]]; then
+      curr_host=$curr_host:$curr_port
+    fi
+
+    if [[ -z "$curr_host" ]]; then
+      printf "Host: "
+    else
+      printf "Host [${curr_host}]: "
+    fi
+    read host
+    if [[ -z "$host" ]]; then
+      host=$curr_host
+    fi
+
+    if [[ -z "$host" ]]; then
+      1>&2 echo "Host cannot be blank"
+      return 1
+    fi
   fi
 
   # Get user
-  local user=
-  local curr_user=$(echo $cur_settings | sed -n "s/.*\/\/\([^@:]*\).*$/\1/p")
-  if [[ -z "$curr_user" ]]; then
-    read -p "Username: " user
-  else
-    read -p "Username [${curr_user}]: " user
-  fi
   if [[ -z "$user" ]]; then
-    user=$curr_user
+    local curr_user=$(echo $curr_settings | sed -n -E "s|${REGEX}|\3|p")
+    [[ -z "$curr_user" ]] && curr_user=$(whoami)
+    if [[ -z "$curr_user" ]]; then
+      printf "Username: "
+    else
+      printf "Username [${curr_user}] (Escape to clear): "
+    fi
+    read user
+    if [[ -z "$user" ]]; then
+      user=$curr_user
+    fi
+    # Check for Esc key
+    if [[ "$user" == $'\e' ]]; then
+      user=
+    fi
   fi
 
   # Get password
   local password=
   if [[ -n "$user" ]]; then
-    read -sp"Please enter your proxy password: " password
+    printf "Please enter your proxy password: "
+    read -s password
     printf "\n"
 
     if [[ -n "$password" ]]; then
-      password=":$(urlencode ${password})"
+      password="$(urlencode ${password})"
     fi
+  fi
+
+  local proxy=${host}
+  if [[ -n "$user" ]] && [[ -n "$password" ]]; then
+    proxy="$user:$password@${host}"
+  elif [[ -n "$user" ]]; then
+    proxy="$user@${host}"
   fi
 
   case $protocol in
     ftp )
-      export ftp_proxy=http://${user}${password}@${host}
+      export ftp_proxy=http://$proxy
       ;;
     http )
-      export http_proxy=http://${user}${password}@${host}
+      export http_proxy=http://$proxy
       ;;
     https )
-      export https_proxy=http://${user}${password}@${host}
+      export https_proxy=http://$proxy
       ;;
     ALL )
-      export ftp_proxy=http://${user}${password}@${host}
-      export http_proxy=http://${user}${password}@${host}
-      export https_proxy=http://${user}${password}@${host}
+      export ftp_proxy=http://$proxy
+      export http_proxy=http://$proxy
+      export https_proxy=http://$proxy
       ;;
   esac
 }
